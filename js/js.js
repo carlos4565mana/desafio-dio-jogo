88c5c600e80172083ba30bc7dadfdbb69ef29b41
carlos4565mana <carloscal61@gmail.com>
2022-03-04 22:42:05 -0300

    created movejogador function

--- js/js.js
+++ js/js.js
@@ -7,6 +7,26 @@ function start() {
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
   var jogo = {}
+
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68
+    }
+  
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+    jogo.pressionou[e.which] = true;
+    });
+  
+  
+    $(document).keyup(function(e){
+         jogo.pressionou[e.which] = false;
+    });
+
 	
 	//Game Loop
 	jogo.timer = setInterval(loop,30);
@@ -14,6 +34,7 @@ function start() {
 	function loop() {
 	
 	movefundo();
+  movejogador();
 	
 	} 
 
@@ -27,5 +48,27 @@ function movefundo() {
 	esquerda = parseInt($("#fundoGame").css("background-position"));
 	$("#fundoGame").css("background-position",esquerda-1);
 	
-	} // fim da função movefundo()
+} // fim da função movefundo()
+
+function movejogador() {
+	
+	if (jogo.pressionou[TECLA.W]) {
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo-10);
+	
+	}
+	
+	if (jogo.pressionou[TECLA.S]) {
+		
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo+10);	
+	}
+	
+	if (jogo.pressionou[TECLA.D]) {
+		
+		//Chama função Disparo	
+	}
+
+	} // fim da função movejogador()
+
 
